43dcd83e2d51856bf44f44b1cd291362232b2a38
Erandi Araujo <erandiaraujo@gmail.com>
2025-03-23 22:45:40 -0400

    Delete

--- DogApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.dogapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.dogapp to javafx.fxml;
-    exports com.example.dogapp;
-}
